5216b11b8f6fc25b76d74f0e066e82cf35e77431
Peter Bødstrup <HO77IY@student.aau.dk>
2025-01-04 13:34:59 +0100

    Lidt ekstra

--- Snake.h
+++ Snake.h
@@ -6,3 +6,34 @@
 #define SNAKE_H
 // Fatter stadig ikke hvordan man bruger en .h rigtigt :'(
 #endif //SNAKE_H
+
+// ++i starter fra 1, hvor i++ starter fra 0.
+
+// %d for int
+// %c for char
+// %lf for double
+// %p for printe det der er gemt i en pointer
+
+struct PackedStruct { // Husk, man skal lægge de forskellige variabler sammen.
+    char a; // 1 byte
+    int b; // 4 bytes
+    double c; // 8 bytes
+};
+
+// En parameter, som optræder i en funktionsdefinition, kaldes en formel parameter. En formel parameter er et navn.
+
+// En parameter, som optræder i et kald af en funktion, kaldes en aktuel parameter. En aktuel parameter er et udtryk, som beregnes inden det overføres
+// Eksempel;
+// type function_name (formal_parameters){
+// declarations
+// commands}
+
+// Forskel på global og lokal. Er om de er defineret i en funktion eller ej, for eksempel hvis de er defineret udenfor "main" er de global.
+
+
+// function_name (actual_parameters)
+
+
+
+// Dereferencing operator(*) used to declare pointer variable and access the value stored in the address.
+// Address operator(&) used to returns the address of a variable or to access the address of a variable to a pointer
